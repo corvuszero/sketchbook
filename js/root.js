0095a9ff98d9e5acfe0d7dcaa03a973b8b014a6a
Alan Morales <alanmorales35@gmail.com>
2016-01-09 23:38:23 -0800

    Basic project structure

---
+++ js/root.js
@@ -0,0 +1,14 @@
+var Sketchbook = React.createClass({
+	render: function() {
+		return (
+			<div className="sketchbook">
+				<h1>Sketchbook</h1>
+			</div>
+		);				
+	}
+});
+
+ReactDOM.render(
+	<Sketchbook />,
+	document.getElementById('root')
+);
