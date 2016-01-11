d17f39c3e2a4c84f0a4ca916870d9f40ec20d4b7
Alan Morales <alanmorales35@gmail.com>
2016-01-10 19:02:36 -0800

    retabbed

--- js/root.js
+++ js/root.js
@@ -1,26 +1,26 @@
 var Pixel = React.createClass({
-	getInitialState: function() {
-		return {
-			pixels: [
-				['#FFFFFF', '#FFFFFF', '#FFFFFF', '#d80000', '#d80000', '#d80000', '#d80000', '#d80000', '#FFFFFF', '#FFFFFF', '#FFFFFF', '#FFFFFF'],
-				['#FFFFFF', '#FFFFFF', '#d80000', '#d80000', '#d80000', '#d80000', '#d80000', '#d80000', '#d80000', '#d80000', '#d80000', '#FFFFFF'],
-				['#FFFFFF', '#FFFFFF', '#706800', '#706800', '#706800', '#f8ab00', '#f8ab00', '#706800', '#f8ab00', '#FFFFFF', '#FFFFFF', '#FFFFFF'],
-				['#FFFFFF', '#706800', '#f8ab00', '#706800', '#f8ab00', '#f8ab00', '#f8ab00', '#706800', '#f8ab00', '#f8ab00', '#f8ab00', '#FFFFFF'],
-				['#FFFFFF', '#706800', '#f8ab00', '#706800', '#706800', '#f8ab00', '#f8ab00', '#f8ab00', '#706800', '#f8ab00', '#f8ab00', '#f8ab00'],
-				['#FFFFFF', '#706800', '#706800', '#f8ab00', '#f8ab00', '#f8ab00', '#f8ab00', '#706800', '#706800', '#706800', '#706800', '#FFFFFF'],
-				['#FFFFFF', '#FFFFFF', '#FFFFFF', '#f8ab00', '#f8ab00', '#f8ab00', '#f8ab00', '#f8ab00', '#f8ab00', '#f8ab00', '#FFFFFF', '#FFFFFF'],
-				['#FFFFFF', '#FFFFFF', '#706800', '#706800', '#d80000', '#706800', '#706800', '#706800', '#FFFFFF', '#FFFFFF', '#FFFFFF', '#FFFFFF'],
-				['#FFFFFF', '#706800', '#706800', '#706800', '#d80000', '#706800', '#706800', '#d80000', '#706800', '#706800', '#706800', '#FFFFFF'],
-				['#706800', '#706800', '#706800', '#706800', '#d80000', '#d80000', '#d80000', '#d80000', '#706800', '#706800', '#706800', '#706800'],
-				['#f8ab00', '#f8ab00', '#706800', '#d80000', '#f8ab00', '#d80000', '#d80000', '#f8ab00', '#d80000', '#706800', '#f8ab00', '#f8ab00'],
-				['#f8ab00', '#f8ab00', '#f8ab00', '#d80000', '#d80000', '#d80000', '#d80000', '#d80000', '#d80000', '#f8ab00', '#f8ab00', '#f8ab00'],
-				['#f8ab00', '#f8ab00', '#d80000', '#d80000', '#d80000', '#d80000', '#d80000', '#d80000', '#d80000', '#d80000', '#f8ab00', '#f8ab00'],
-				['#FFFFFF', '#FFFFFF', '#d80000', '#d80000', '#d80000', '#FFFFFF', '#FFFFFF', '#d80000', '#d80000', '#d80000', '#FFFFFF', '#FFFFFF'],
-				['#FFFFFF', '#706800', '#706800', '#706800', '#FFFFFF', '#FFFFFF', '#FFFFFF', '#FFFFFF', '#706800', '#706800', '#706800', '#FFFFFF'],
-				['#706800', '#706800', '#706800', '#706800', '#FFFFFF', '#FFFFFF', '#FFFFFF', '#FFFFFF', '#706800', '#706800', '#706800', '#706800']
-			]
-		}
-	},
+  getInitialState: function() {
+    return {
+      pixels: [
+        ['#FFFFFF', '#FFFFFF', '#FFFFFF', '#d80000', '#d80000', '#d80000', '#d80000', '#d80000', '#FFFFFF', '#FFFFFF', '#FFFFFF', '#FFFFFF'],
+        ['#FFFFFF', '#FFFFFF', '#d80000', '#d80000', '#d80000', '#d80000', '#d80000', '#d80000', '#d80000', '#d80000', '#d80000', '#FFFFFF'],
+        ['#FFFFFF', '#FFFFFF', '#706800', '#706800', '#706800', '#f8ab00', '#f8ab00', '#706800', '#f8ab00', '#FFFFFF', '#FFFFFF', '#FFFFFF'],
+        ['#FFFFFF', '#706800', '#f8ab00', '#706800', '#f8ab00', '#f8ab00', '#f8ab00', '#706800', '#f8ab00', '#f8ab00', '#f8ab00', '#FFFFFF'],
+        ['#FFFFFF', '#706800', '#f8ab00', '#706800', '#706800', '#f8ab00', '#f8ab00', '#f8ab00', '#706800', '#f8ab00', '#f8ab00', '#f8ab00'],
+        ['#FFFFFF', '#706800', '#706800', '#f8ab00', '#f8ab00', '#f8ab00', '#f8ab00', '#706800', '#706800', '#706800', '#706800', '#FFFFFF'],
+        ['#FFFFFF', '#FFFFFF', '#FFFFFF', '#f8ab00', '#f8ab00', '#f8ab00', '#f8ab00', '#f8ab00', '#f8ab00', '#f8ab00', '#FFFFFF', '#FFFFFF'],
+        ['#FFFFFF', '#FFFFFF', '#706800', '#706800', '#d80000', '#706800', '#706800', '#706800', '#FFFFFF', '#FFFFFF', '#FFFFFF', '#FFFFFF'],
+        ['#FFFFFF', '#706800', '#706800', '#706800', '#d80000', '#706800', '#706800', '#d80000', '#706800', '#706800', '#706800', '#FFFFFF'],
+        ['#706800', '#706800', '#706800', '#706800', '#d80000', '#d80000', '#d80000', '#d80000', '#706800', '#706800', '#706800', '#706800'],
+        ['#f8ab00', '#f8ab00', '#706800', '#d80000', '#f8ab00', '#d80000', '#d80000', '#f8ab00', '#d80000', '#706800', '#f8ab00', '#f8ab00'],
+        ['#f8ab00', '#f8ab00', '#f8ab00', '#d80000', '#d80000', '#d80000', '#d80000', '#d80000', '#d80000', '#f8ab00', '#f8ab00', '#f8ab00'],
+        ['#f8ab00', '#f8ab00', '#d80000', '#d80000', '#d80000', '#d80000', '#d80000', '#d80000', '#d80000', '#d80000', '#f8ab00', '#f8ab00'],
+        ['#FFFFFF', '#FFFFFF', '#d80000', '#d80000', '#d80000', '#FFFFFF', '#FFFFFF', '#d80000', '#d80000', '#d80000', '#FFFFFF', '#FFFFFF'],
+        ['#FFFFFF', '#706800', '#706800', '#706800', '#FFFFFF', '#FFFFFF', '#FFFFFF', '#FFFFFF', '#706800', '#706800', '#706800', '#FFFFFF'],
+        ['#706800', '#706800', '#706800', '#706800', '#FFFFFF', '#FFFFFF', '#FFFFFF', '#FFFFFF', '#706800', '#706800', '#706800', '#706800']
+      ]
+    }
+  },
 
   render: function() {
     return (
@@ -32,27 +32,27 @@ var Pixel = React.createClass({
           </div>
         </div>
       </div>
-		);
-	}
+    );
+  }
 });
 
 var PixelCanvas = React.createClass({
-	componentDidMount: function() {
+  componentDidMount: function() {
     window.addEventListener('resize', this.handleResize);
 
-		// Drawing from the get-go
-		this.forceUpdate();
-	},
+    // Drawing from the get-go
+    this.forceUpdate();
+  },
 
   componentWillUnmount: function() {
     window.removeEventListener('resize', this.handleResize);                      
   },
 
-	draw: function () {
-		var canvas = this.refs.canvas || null;
-		if (canvas == null) {
-			return;
-		}
+  draw: function () {
+    var canvas = this.refs.canvas || null;
+    if (canvas == null) {
+      return;
+    }
     /*
      * Canvas always has a default size of 300 x 150. Overriding.
      * http://www.w3.org/TR/2012/WD-html5-author-20120329/the-canvas-element.html#the-canvas-element
@@ -60,42 +60,42 @@ var PixelCanvas = React.createClass({
     canvas.width = canvas.offsetWidth;
     canvas.height = canvas.offsetHeight;
 
-		var ctx = canvas.getContext('2d');
+    var ctx = canvas.getContext('2d');
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-		var pixels = this.props.pixels;
-		// Calculating pixel size to keep aspect ratio of drawing
-		var size = Math.floor(Math.min(
-			canvas.width / pixels[0].length, 
-			canvas.height / pixels.length
-		));
-		var offsetX = (canvas.width - (size * pixels[0].length)) / 2;
-		var offsetY = (canvas.height - (size * pixels.length)) / 2;
-		for (var y = 0; y < pixels.length; y++) {
-			for (var x = 0; x < pixels[y].length; x++) {
-				ctx.fillStyle = pixels[y][x];
-				ctx.fillRect(
+    var pixels = this.props.pixels;
+    // Calculating pixel size to keep aspect ratio of drawing
+    var size = Math.floor(Math.min(
+      canvas.width / pixels[0].length, 
+      canvas.height / pixels.length
+    ));
+    var offsetX = (canvas.width - (size * pixels[0].length)) / 2;
+    var offsetY = (canvas.height - (size * pixels.length)) / 2;
+    for (var y = 0; y < pixels.length; y++) {
+      for (var x = 0; x < pixels[y].length; x++) {
+        ctx.fillStyle = pixels[y][x];
+        ctx.fillRect(
           Math.floor(offsetX + (x * size)), 
           Math.floor(offsetY + ( y * size)), 
           size, 
           size
         );
-			}
-		}
-	},
+      }
+    }
+  },
 
   handleResize: function(e) {
     this.draw();
   },
 
-	render: function() {
-		this.draw();
+  render: function() {
+    this.draw();
 
-		return (
-			<canvas ref="canvas" className="pixelCanvas">
-			</canvas>
-		);	
-	}
+    return (
+      <canvas ref="canvas" className="pixelCanvas">
+      </canvas>
+    );  
+  }
 });
 
 var PixelMenu = React.createClass({
@@ -109,16 +109,16 @@ var PixelMenu = React.createClass({
 });
 
 var Sketchbook = React.createClass({
-	render: function() {
-		return (
-			<div className="container">
-				<Pixel />
-			</div>
-		);
-	}
+  render: function() {
+    return (
+      <div className="container">
+        <Pixel />
+      </div>
+    );
+  }
 });
 
 ReactDOM.render(
-	<Sketchbook />,
-	document.getElementById('root')
+  <Sketchbook />,
+  document.getElementById('root')
 );
