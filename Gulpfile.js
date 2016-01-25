a49ba06b99239a8ba4229403227009587a6ee2c9
Alan Morales <alanmorales35@gmail.com>
2016-01-24 16:24:02 -0800

    Gulp watch task

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,16 +1,21 @@
 const babelify = require('babelify');
 const browserify = require('gulp-browserify');
+const concat = require('gulp-concat');
+const htmlreplace = require('gulp-html-replace');
 const gulp = require('gulp');
-const resolutions = require('browserify-resolutions');
+const uglify = require('gulp-uglify');
 
-gulp.task('scripts', function() {
+gulp.task('pack', function() {
   gulp.src(['app/main.js'])
     .pipe(browserify({
       debug: true,
-      transform: [babelify.configure({presets: ['es2015', 'react']})],
-      plugin: [[resolutions, '*']]
+      transform: [babelify.configure({presets: ['es2015', 'react']})]
     }))
     .pipe(gulp.dest('./public/'));
 });
 
-gulp.task('default', ['scripts']);
+gulp.task('watch', function() {
+  gulp.watch(['app/components/*.js', 'app/components/**/*.js'], ['pack']);
+});
+
+gulp.task('default', ['pack', 'watch']);
