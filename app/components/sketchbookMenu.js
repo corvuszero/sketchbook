81cf8b7c4daeb50aedc86f1d8d8b2ae9b6ad3d29
Alan Morales <alanmorales35@gmail.com>
2016-01-30 19:38:46 -0800

    Menu improvements

--- app/components/sketchbookMenu.js
+++ app/components/sketchbookMenu.js
@@ -1,12 +1,15 @@
 'use strict';
 
+const MenuSection = require('./MenuSection');
 const React = require('react');
 
 const SketchbookMenu = React.createClass({
   render: function() {
     return (
       <div className="menu">
-        <h1 className="title">{this.props.title}</h1>
+        <MenuSection>
+          <div className="title">{this.props.title}</div>
+        </MenuSection>
         {this.props.children}
       </div>
     );
